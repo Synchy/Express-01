bfc131090c59f7d95fd46bc925adb8a21f8c47bf
flora cesarini <flo.cesarini@gmail.com>
2022-07-21 09:32:30 +0200

    1st thing

---
+++ index.js
@@ -0,0 +1,68 @@
+const express = require("express");
+
+const app = express();
+
+const port = 5000;
+
+const welcome = (req, res) => {
+  res.send("welcome to my favorite Movie List");
+};
+
+app.get("/", welcome);
+
+
+const movies = [
+  {
+    id: 1,
+    title: "Citizen Kane",
+    director: "Orson Wells",
+    year: "1941",
+    colors: false,
+    duration: 120,
+  },
+  {
+    id: 2,
+    title: "The Godfather",
+    director: "Francis Ford Coppola",
+    year: "1972",
+    colors: true,
+    duration: 180,
+  },
+  {
+    id: 3,
+    title: "Pulp Fiction",
+    director: "Quentin Tarantino",
+    year: "1994",
+    color: true,
+    duration: 180,
+  },
+];
+
+const getMovies = (req, res) => {
+  res.status(200).json(movies);
+};
+
+app.get("/api/movies", getMovies);
+
+
+
+let uniqueMovie = (req, res) => { 
+  const result = movies.find((movie) => movie.id == req.params.id);
+    if (result)  res.status(200).json(result);
+
+    if (!result) res.status(404).send('Cannot find the page');
+    
+  };
+
+app.get("/api/movies/:id", uniqueMovie);
+
+
+app.listen(port, (err) => {
+  if (err) {
+    console.error("Something bad happened");
+  } else {
+    console.log(`Server is listening on ${port}`);
+  }
+});
+
+
